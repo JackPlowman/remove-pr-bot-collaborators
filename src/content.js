620265a56834a56972c7f5e793f551e2303ec426
Jack Plowman <62281988+JackPlowman@users.noreply.github.com>
2025-08-22 20:45:18 +0100

    update

--- src/content.js
+++ src/content.js
@@ -6,7 +6,7 @@ const DEFAULT_REGEX_SOURCES = [
   "\\[bot\\]",
   // Matches tokenized bot names like -bot, +bot, .bot or bot end
   "(?:^|[+\\-._])bot(?:$|[+\\-._])",
-  // Matches GitHub Copilot co-author lines,
+  // Matches GitHub Copilot co-author lines
   "Copilot",
 ];
 
